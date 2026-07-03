362be56a9542917a154d0b83f36dfbcec0c114e4
Raynor Ananta <79029536+rayytsn9@users.noreply.github.com>
2026-07-03 03:48:11 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,18 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 2 pointers approach
+
+        s_new = re.sub(r'[^a-zA-Z0-9]', '', s)
+
+        l, r = 0, len(s_new) - 1
+
+        while l < r:
+            if s_new[l].lower() != s_new[r].lower():
+                return False
+
+            l, r = l + 1, r - 1
+
+        return True
+
