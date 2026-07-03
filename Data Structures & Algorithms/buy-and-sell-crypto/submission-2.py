1266eed434fb82f867459f0680f260b344c038d1
Raynor Ananta <79029536+rayytsn9@users.noreply.github.com>
2026-07-03 03:23:26 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        if not prices:
+            return 0
+
+        min = None
+        max = 0
+
+        for price in prices:
+
+            if min is None or price < min:
+                min = price
+            elif price - min > max:
+                max = price - min
+
+        return max
